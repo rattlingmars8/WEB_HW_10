a5d624a2148989171d02fc5c262aae66961ef924
rattlingmars8 <rattlingmars8@gmail.com>
2023-06-21 20:22:53 +0200

    Initial commit

---
+++ HW_10/quotes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .utils import get_mongodb
+
+
+def main(request, page=1):
+    db = get_mongodb()
+    quotes = db.quotes.find()
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page)
+    quotes_on_page = paginator.page(page)
+    return render(request, "quotes/index.html", context={"quotes": quotes_on_page})
