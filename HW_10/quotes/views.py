2d2616dcfffe487fead06739ad0f9b4f3f28a04d
rattlingmars8 <rattlingmars8@gmail.com>
2023-06-23 22:33:38 +0200

    Functional tags paginatopn

--- HW_10/quotes/views.py
+++ HW_10/quotes/views.py
@@ -11,12 +11,12 @@ def main(request, page=1):
     per_page = 10
     paginator = Paginator(quotes, per_page)
     quotes_on_page = paginator.get_page(page)
-    return render(request, "quotes/index.html", context={"quotes": quotes_on_page})
+    return render(request, "quotes/index.html", context={"quotes": quotes_on_page, "url_templates": "/"})
 
 
-def tags_page(request, tag, page=None):
+def tags_page(request, tag, page=1):
     quotes = Quotes.objects.filter(tags__name=tag)
     per_page = 10
     paginator = Paginator(quotes, per_page)
     quotes_on_page = paginator.get_page(page)
-    return render(request, "quotes/tags_index.html", context={"quotes": quotes_on_page})
+    return render(request, "quotes/index.html", context={"quotes": quotes_on_page, "url_templates": "tag", "tag": tag})
